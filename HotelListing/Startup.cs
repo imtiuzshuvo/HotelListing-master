0330ba8e4e25b9deeb05d4be2acf6f76031cb75a
Imtiuz Shuvo <imtiuz@gmail.com>
2022-01-12 13:11:30 +0600

    added EF core, sql server, tools plugins; added Hotel and Country data classes; added DatabaseContext class; added connectionstring in the appsettings; added DbContext service as our DatabaseContext; added migration and created database

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -40,6 +42,10 @@ namespace HotelListing
                     builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
                 );
             });
+
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlServerConnectionString"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
