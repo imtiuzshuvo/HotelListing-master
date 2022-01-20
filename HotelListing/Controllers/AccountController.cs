3caff2aebf1be587e9cbc9bd5f58ba9b69c977a4
Imtiuz Shuvo <imtiuz@gmail.com>
2022-01-20 12:19:40 +0600

    Added user registration endpoint by adding AccountController, UserDTO, and mapping configuration

---
+++ HotelListing/Controllers/AccountController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<APIUser> _userManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IMapper _mapper;
+
+        public AccountController(
+                UserManager<APIUser> userManager,
+                ILogger<AccountController> logger,
+                IMapper mapper
+            )
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Registration attempt for {userDTO.Email} ");
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var user = _mapper.Map<APIUser>(userDTO);
+                user.UserName = userDTO.Email;
+                var result = await _userManager.CreateAsync(user, userDTO.Password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    _logger.LogInformation("Registration attempt failed");
+                    return BadRequest(ModelState);
+                }
+
+                _logger.LogInformation("Registration successful.");
+                return Accepted("Registation successful.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(Register)}");
+                return Problem($"Something went wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+        //[HttpPost]
+        //[Route("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginUserDTO userDTO)
+        //{
+        //    _logger.LogInformation($"Login attempt for {userDTO.Email} ");
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    try
+        //    {
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}");
+        //        return Problem($"Something went wrong in the {nameof(Login)}", statusCode: 500);
+        //    }
+        //}
+    }
+}
